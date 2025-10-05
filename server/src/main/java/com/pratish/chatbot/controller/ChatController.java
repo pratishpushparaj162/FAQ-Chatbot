72c417e111705cc1aa7e92ae31a1e32b51378fa3
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-04 23:16:35 -0400

    Update ChatController.java

--- server/src/main/java/com/pratish/chatbot/controller/ChatController.java
+++ server/src/main/java/com/pratish/chatbot/controller/ChatController.java
@@ -6,7 +6,7 @@ import com.pratish.chatbot.service.ChatService;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
+ 
 @RestController
 @RequestMapping("/api/chat")
 @CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
